ed10803d877ef6bd9eafc25ce26226e95f41a2a7
ValentinoDiaz0509 <166324234+ValentinoDiaz0509@users.noreply.github.com>
2025-06-21 17:39:46 -0300

    Add files via upload

---
+++ src/Clases/Administrativo.java
@@ -0,0 +1,64 @@
+package Clases;
+
+import java.util.List;
+
+public class Administrativo extends Empleado {
+
+    // Constructor
+    public Administrativo(int id, String nombre) {
+        super(id, nombre);
+    }
+
+    // ingresarEntradaDeVehiculos toma como parámetro plaza y vehiculo.
+    public void ingresarEntradaDeVehiculos(PlazaDeAparcamiento plaza, Vehiculo vehiculo) {
+        
+
+        if (plaza == null) {
+            System.out.println("Error: No hay una plaza de aparcamiento.");
+            return;
+        }
+
+        if (vehiculo == null) {
+            System.out.println("Error: No hay un vehículo.");
+            return;
+        }
+
+        if (plaza.estaOcupada()) {
+            System.out.println("Error: La plaza " + plaza.getNumero() + " ya está ocupada.");
+            return;
+        }
+
+        plaza.ocuparPlaza(vehiculo);
+        System.out.println("Vehículo " + vehiculo.getPatente() + " ingresado en la plaza " + plaza.getNumero() + ".");
+    }
+
+    
+    
+    public void registrarSalidaDeVehiculos(PlazaDeAparcamiento plaza) {
+        
+
+        if (plaza == null) {
+            System.out.println("Error: No se indicó una plaza de aparcamiento.");
+            return;
+        }
+
+        if (!plaza.estaOcupada()) {
+            System.out.println("Información: La plaza " + plaza.getNumero() + " ya se encontraba libre.");
+            return;
+        }
+
+        Vehiculo vehiculo = plaza.getVehiculoEstacionado();
+        
+        plaza.desocuparPlaza();
+        
+        System.out.println("Vehículo salió de la plaza.");
+    }
+
+    
+    
+    // verDisponibilidadDePlazas revisa el estado de una plaza
+    public void verDisponibilidadDePlazas() {
+    	PlazaDeAparcamiento.mostrarEstadoPlazas();
+    }
+    
+}
---
+++ src/Clases/Cliente.java
@@ -0,0 +1,49 @@
+package Clases;
+
+public class Cliente {
+
+    private String nombre;
+    private String dni;
+    private String telefono;
+    private Vehiculo vehiculo;
+
+    public Cliente(String nombre, String dni, String telefono, Vehiculo vehiculo) {
+        this.nombre = nombre;
+        this.dni = dni;
+        this.telefono = telefono;
+        this.vehiculo = vehiculo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public Vehiculo getVehiculo() {
+        return vehiculo;
+    }
+
+    public void setVehiculo(Vehiculo vehiculo) {
+        this.vehiculo = vehiculo;
+    }
+    
+}
